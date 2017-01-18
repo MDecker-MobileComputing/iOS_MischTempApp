36fab48cf1ca6dfc06d6148cb4f087fbbc5db846
MDecker (auf MacOS) <michi@luftapfel.fritz.box>
2017-01-18 20:23:33 +0100

    In Playground mit Beispielen von Framework-Klassen noch Variante mit NSError dazu

--- BeispieleVonFrameworkKlassen.playground/Contents.swift
+++ BeispieleVonFrameworkKlassen.playground/Contents.swift
@@ -9,12 +9,28 @@ import Foundation
 
 let einString = "Dieser String soll in eine Datei geschrieben werden."
 do {
-  try einString.write(toFile:"datei.txt", atomically: true, encoding: .utf8)
+  try einString.write(toFile:"datei-1.txt", atomically: true, encoding: .utf8)
   print("String wurde in Datei im Sandbox-Container geschrieben.")
 }
 catch {
   print("Exception beim Schreiben einer Datei aufgetreten: \(error)")
 }
+
 // Playground hat nur auf ein Sandbox-Container-Dateisystem Zugriff,
 // Datei wird z.B. geschrieben unter:
 // /var/folders/63/26kb00c11ybfl5ww4ntpwkkm0000gn/T/com.apple.dt.Xcode.pg/containers/com.apple.dt.playground.stub.iOS_Simulator.BeispieleVonFrameworkKlassen-9A7E6C95-B85D-46E3-983E-0963EC5D0DEC/datei.txt
+
+
+// Selbes Beispiel, aber jetzt mit NSError-objekt, das Zusatz-Infos enthalten kann
+
+let nochEinString = "Dieser String soll auch n eine Datei geschrieben werden."
+do {
+    try einString.write(toFile:"datei-2.txt", atomically: true, encoding: .utf8)
+    print("Zweiter String wurde in Datei im Sandbox-Container geschrieben.")
+}
+catch let nserr as NSError{
+    print("Exception beim Schreiben einer Datei aufgetreten: \(nserr.localizedDescription)")
+}
+
+
+
